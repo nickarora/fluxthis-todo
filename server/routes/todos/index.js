292537172fe7c1edfe28d87ba689fa5fef5d6676
Nick Arora <arora.nick@gmail.com>
2016-05-05 15:02:08 -0700

    create remaining routes for todos

--- server/routes/todos/index.js
+++ server/routes/todos/index.js
@@ -11,4 +11,30 @@ router.get('/', (req, res, next) =>
     )
 )
 
+router.put('/', ({ todo }, res, next) =>
+  Todo.findOneAndUpdate(
+    { _id: todo.id },
+    { note: todo.note, complete: todo.complete }
+  ).then(
+    updatedTodo => res.status(200).json(updatedTodo),
+    err => next(err)
+  )
+)
+
+router.post('/', ({ todo }, res, next) =>
+  Todo.insert(todo)
+  .then(
+    insertedTodo => res.status(200).json(insertedTodo),
+    err => next(err)
+  )
+)
+
+router.delete('/', ({ todo }, res, next) =>
+  Todo.remove({ _id: todo.id })
+  .then(
+    deletedTodo => res.status(200).json(deletedTodo),
+    err => next(err)
+  )
+)
+
 export default router
